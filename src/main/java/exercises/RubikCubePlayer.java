9d3dfa0afdcb4faffda3cb78a0874824edadbf32
oscarfndez <oscarfndez@gmail.com>
2016-02-05 14:09:15 +0000

    bla bla

--- src/main/java/exercises/RubikCubePlayer.java
+++ src/main/java/exercises/RubikCubePlayer.java
@@ -24,64 +24,89 @@ public class RubikCubePlayer {
         }
     }
 
-    enum Direction {
+    enum MovementType {
 
-        UP,
-        DOWN,
-        RIGHT,
-        LEFT
-    }
-
-    class Side {
-
-        public int code;
-        public Adjacency left;
-        public Adjacency right;
-        public Adjacency up;
-        public Adjacency down;
-        public Adjacency opposite;
-
-        Side(int code, Adjacency left, Adjacency right, Adjacency up, Adjacency down, Adjacency opposite) {
-
-            this.code = code;
-            this.left = left;
-            this.right = right;
-            this.up = up;
-            this.down = down;
-            this.opposite = opposite;
-        }
+        VERTICAL,
+        HORIZONTAL,
+        ROTATION
     }
 
     class Movement {
-
         public int line;
-        public Direction direction;
+        public MovementType direction;
     }
 
-    enum Rotation {
-        D0,
-        D90,
-        D180,
-        D270,
-        D360
-    }
-
-     class Adjacency {
-
-        public Side side;
-        public Rotation rotation;
-
-         Adjacency(Side sideCode, Rotation rotation) {
-            this.side = side;
-            this.rotation = rotation;
-        }
-    }
 
     public static void main(String args[]) {
 
         RubikCubePlayer rubikCubePlayer = new RubikCubePlayer();
         Color[][][] rubikCube = rubikCubePlayer.randomizeStartingPoint();
-        rubikCubePlayer.printRubikCube(rubikCube);
+
+        boolean finished = false;
+
+        while (!finished) {
+            rubikCubePlayer.printRubikCube(rubikCube);
+            Movement nextMovement = rubikCubePlayer.randomizeMovement();
+            rubikCubePlayer.applyMovement(rubikCube, nextMovement);
+        }
+    }
+
+    private Movement randomizeMovement() {
+
+        Movement movement = new Movement();
+        movement.line = ThreadLocalRandom.current().nextInt(ROWS);
+        movement.direction = Arrays.asList(MovementType.values()).get(ThreadLocalRandom.current().nextInt(NUM_COLORS));
+        return movement;
+    }
+
+    private void applyMovement(Color[][][] rubikCube, Movement movement) {
+
+        Color[][][] originalCopy = arrayCopy(rubikCube);
+
+        switch (movement.direction) {
+            case VERTICAL:
+                applyVerticalMovement(originalCopy, rubikCube, 0, 3, movement.line);
+                applyVerticalMovement(originalCopy, rubikCube, 3, 2, movement.line);
+                applyVerticalMovement(originalCopy, rubikCube, 2, 1, movement.line);
+                applyVerticalMovement(originalCopy, rubikCube, 1, 0, movement.line);
+
+                //Missing the possible rotation
+            case HORIZONTAL:
+                applyHorizontalMovement(originalCopy, rubikCube, 0, 5, movement.line);
+                applyHorizontalMovement(originalCopy, rubikCube, 5, 2, movement.line);
+                applyHorizontalMovement(originalCopy, rubikCube, 2, 4, movement.line);
+                applyHorizontalMovement(originalCopy, rubikCube, 4, 0, movement.line);
+
+                //Missing the possible rotation
+            case ROTATION:
+        }
+    }
+
+    private void applyVerticalMovement(Color[][][] originalCube, Color[][][] destinationCube, int originalSide, int destinationSide, int line) {
+
+        destinationCube[destinationSide][line][0] = originalCube[originalSide][line][0];
+        destinationCube[destinationSide][line][1] = originalCube[originalSide][line][1];
+        destinationCube[destinationSide][line][2] = originalCube[originalSide][line][2];
+    }
+
+    private void applyHorizontalMovement(Color[][][] originalCube, Color[][][] destinationCube, int originalSide, int destinationSide, int line) {
+
+        destinationCube[destinationSide][0][line] = originalCube[originalSide][0][line];
+        destinationCube[destinationSide][1][line] = originalCube[originalSide][1][line];
+        destinationCube[destinationSide][2][line] = originalCube[originalSide][2][line];
+    }
+
+    public Color[][][] arrayCopy(Color[][][] src) {
+
+        Color[][][] dst = new Color[ROWS][ROWS][ROWS];
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                for (int k = 0; k < ROWS; k++) {
+                    dst[i][j][k] = src[i][j][k];
+                }
+            }
+        }
+        return dst;
     }
 
     private Color[][][] randomizeStartingPoint() {
@@ -132,22 +157,5 @@ public class RubikCubePlayer {
             }
         }
         return true;
-    }
-
-    public void rotate(Side side, Color[][][] rubikCube, Movement rotation) {
-
-        for (Side s : Side.values()) {
-            computeAffectation(side, s, rubikCube, rotation);
-        }
-    }
-
-    private void computeAffectation(Side originator, Side affectedSide, Color[][][] rubikCube, Movement rotation) {
-
-        switch (rotation.direction) {
-            case UP:
-            case DOWN:
-            case RIGHT:
-            case LEFT:
-        }
     }
 }
